9bbdc04dd68987d7f64ba607278cf3ec08341c3b
Maria <mariacarolina13492@gmail.com>
2022-08-23 22:44:45 -0300

    mensagem de commit

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,11 @@
-let nome = Maria
-let email = mariacarolina13492@gmail.com
-let profissao = analistaAdministrativo
-let idade = 25
-let pais = italia
+let nome = "maria"
+let email = "mariacarolina13492"
+let profissao = "analistaAdministrativ"
+let idade = "25"
+let pais = "italia"
+
+console.log (nome)
+console.log (email)
+console.log (profissao)
+console.log (idade)
+console.log (pais)
