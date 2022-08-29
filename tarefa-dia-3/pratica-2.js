f58d9eccfdd60410ff4697025396c0d93b76edac
Maria <mariacarolina13492@gmail.com>
2022-08-28 22:38:28 -0300

    it worked

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,20 @@
+let multiplicador = 1
+let n = 10
+let i = 1
+
+
+while (i <= n) {
+    let resultado = multiplicador * i
+   
+    console.log (multiplicador + "X" , i , "=" + resultado)
+    i++
+}
+
+multiplicadorA = multiplicador + 1
+
+while (i <= n) {
+    let resultado = multiplicadorA * i
+   
+    console.log (multiplicadorA + "X" , i , "=" + resultado)
+    i++
+}
